49c388f93d0bf76a60e7fd2fe79b870fdb89a0a2
Tejas Thiyagarajan <tejast@Tejass-MacBook-Pro.local>
2019-08-07 11:42:56 -0700

    Changes

--- org/firstinspires/ftc/teamcode/bob.java
+++ org/firstinspires/ftc/teamcode/bob.java
@@ -1,17 +1,5 @@
 public class bob {
   public static void main(String[] args){
-    System.out.println("Hello");
-    System.out.println("AB Playz");
-    System.out.println("Hello");
-    //hello everyone
-  }
 
-  class Human{
-    String skin_color;
-    public Human(String skin_color){
-      this.skin_color = skin_color;
-      this.stupidity = Arul;
     }
   }
-
-}
