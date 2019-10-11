33ec1d9a098a13cfc6d032d8c2d89ca336c915fd
Tejas <tejas.thiyagarajan@gmail.copm>
2019-10-11 13:42:38 -0700

    Bug fixes

--- org/firstinspires/ftc/teamcode/EncoderDrive.java
+++ org/firstinspires/ftc/teamcode/EncoderDrive.java
@@ -31,7 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
